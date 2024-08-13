f57f5546aacd814eaafe4bdc98599e577a11d649
Elle <ellebolt@me.com>
2024-08-13 17:56:21 +0200

    Initial setup of WooCommerce plugin

---
+++ woocommerce-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Product Creation Assistant
+Plugin URI: https://yourwebsite.com/
+Description: A custom WooCommerce plugin.
+Version: 1.0
+Author: Elle Bolt
+Author URI: https://yourwebsite.com/
+License: GPL2
+Text Domain: woocommerce-plugin
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Start coding your plugin here...
